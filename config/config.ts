926835eee9c6b04c6fc5859d0709adb8030030d3
karel kremer <karel.kremer@redpencil.io>
2026-03-31 10:37:55 +0200

    add config for expression labels

--- config/config.ts
+++ config/config.ts
@@ -50,6 +50,61 @@ export default {
         BIND(IF(BOUND(?directTitle), ?directTitle, ?annotatedTitle) AS ?title)
       `,
     },
+    'expression-label': {
+      label: 'Expression Labels',
+      // free
+      // PREFIX oa: <http://www.w3.org/ns/oa#>
+      // PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+      prefixes: `
+        PREFIX eli: <http://data.europa.eu/eli/ontology#>
+      `,
+      // can also use to filter ?annotation in case we want to filter the kind of annotations to show
+      // note that we have to filter by expressions having a work because other expressions are created
+      // by the ai flow (translations etc)
+      // filtering on expressions that have som sort of title too (regular or annotation)
+      targetFilter: `
+        ?target a eli:Expression .
+        ?work eli:is_realized_by ?target .
+        FILTER (BOUND(?title))
+      `,
+      // can use to filter annotations for a given target, need to fix the set of agents once we have final uris for them
+      annotationFilter: `
+        VALUES ?agent {
+          <http://example.org/model_annotation>
+        }
+      `,
+
+      // TODO this should likely still be the default structure?
+      // annotationPath: `
+      //   ?annotation oa:hasTarget ?resource .
+      //   ?resource oa:source / ^eli:is_realized_by? ?target .
+      // `,
+
+      annotationPath: `
+        ?annotation oa:hasTarget / ^eli:is_realized_by? ?target .
+      `,
+      filters: {
+        owner: {
+          query: `
+            ?target oa:hasBody ?body .
+            VALUES ?body { $filterValue } 
+          `,
+          type: 'uri[]',
+        },
+      },
+      titlePath: `
+        OPTIONAL {
+          ?target eli:title ?directTitle .
+        }
+        OPTIONAL {
+          ?target ^oa:hasTarget / oa:hasBody ?body .
+          ?body rdf:predicate eli:title .
+          ?body rdf:object ?annotatedTitle .
+        }
+        BIND(IF(BOUND(?directTitle), ?directTitle, ?annotatedTitle) AS ?title)
+      `,
+    },
   },
   valueTypes: {
     'http://xmlns.com/foaf/0.1/Person': {
