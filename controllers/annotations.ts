c6e84bb6f2e1b897da1ba3f83c86c77b93bbe745
karel kremer <karel.kremer@redpencil.io>
2026-04-02 13:02:23 +0200

    fix rebase

--- controllers/annotations.ts
+++ controllers/annotations.ts
@@ -121,12 +121,6 @@ async function getAnnotationsData(
   pageSize: number,
   targetId?: string,
 ) {
-  let targetValueFilter = '';
-  if (targetId) {
-    targetValueFilter = `VALUES ?targetId {
-      ${sparqlEscapeString(targetId)}
-    }`;
-  }
   const offset = page * pageSize;
   const result = await query(`
     ${target.prefixes}
