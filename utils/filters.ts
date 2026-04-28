c19e8d2773ef3f103ef0e0df07dbf7c5d6d78e17
karel kremer <karel.kremer@redpencil.io>
2026-04-28 09:11:07 +0200

    linting

--- utils/filters.ts
+++ utils/filters.ts
@@ -16,6 +16,7 @@ export function buildFilterString(target: Target, filters: Filters) {
     `;
 
     if (filterConfig.type === 'search') {
+      // eslint-disable-next-line
       const safeValue = filters[key].split("'").join('').split('"').join('');
       filterString =
         filterString.split(`$${filterConfig.variable}`).join(safeValue) + '\n}';
